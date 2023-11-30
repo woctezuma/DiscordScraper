f852cad3e00a251c056ca51a9df56b2010ce3396
Wok <woctezuma@users.noreply.github.com>
2023-11-30 23:12:42 +0100

    Add main()

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from internal.selfbot import client
 from internal.utils import Logger, check_config_file, get_account_settings, show_header
 
-show_header()
-check_config_file()
-log = Logger()
-config = get_account_settings()
 
-client.run(config["token"])
+def main() -> None:
+    show_header()
+    check_config_file()
+    Logger()
+    config = get_account_settings()
+
+    client.run(config["token"])
+
+
+if __name__ == "__main__":
+    main()
