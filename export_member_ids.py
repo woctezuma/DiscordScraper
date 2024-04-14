c1633e369a53f73746068f1ff376bd80bfd102a1
Wok <woctezuma@users.noreply.github.com>
2024-04-14 10:24:36 +0200

    Add a script to export member IDs

---
+++ export_member_ids.py
@@ -0,0 +1,9 @@
+from src.member_utils import export_member_ids_to_txt
+
+
+def main() -> None:
+    export_member_ids_to_txt()
+
+
+if __name__ == "__main__":
+    main()
--- src/member_utils.py
+++ src/member_utils.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 from internal.constants import MEMBER_LIST_FNAME, OUTPUT_FOLDER_NAME
+from internal.utils import MEMBER_ID_FNAME
 
 
 def load_member_dictionaries() -> dict:
@@ -10,3 +11,22 @@ def load_member_dictionaries() -> dict:
         with Path(fname).open() as f:
             d.update(json.load(f))
     return d
+
+
+def parse_member_ids() -> list[str]:
+    d = load_member_dictionaries()
+    return list(d.keys())
+
+
+def export_member_ids_to_txt(output_fname: str = "") -> None:
+    if not output_fname:
+        output_fname = MEMBER_ID_FNAME
+
+    member_ids = sorted(parse_member_ids(), key=int)
+
+    with Path(output_fname).open("w") as f:
+        f.write("\n".join(member_ids))
+
+
+if __name__ == "__main__":
+    export_member_ids_to_txt()
