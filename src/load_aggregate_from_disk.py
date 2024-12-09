a921deecce6d81e76594129607fe9b424e17f840
Wok <woctezuma@users.noreply.github.com>
2024-12-09 17:06:33 +0100

    Load monitored content from disk

--- src/load_aggregate_from_disk.py
+++ src/load_aggregate_from_disk.py
@@ -1,7 +1,11 @@
 import json
 from pathlib import Path
 
-from internal.constants import AGGREGATED_PROFILES_FNAME, OUTPUT_FOLDER_NAME
+from internal.constants import (
+    AGGREGATED_PROFILES_FNAME,
+    MONITORED_CONTENT_FNAME,
+    OUTPUT_FOLDER_NAME,
+)
 
 
 def load_aggregate_from_disk(output_fname: str = AGGREGATED_PROFILES_FNAME) -> dict:
@@ -13,3 +17,8 @@ def load_aggregate_from_disk(output_fname: str = AGGREGATED_PROFILES_FNAME) -> d
     except FileNotFoundError:
         aggregate = {}
     return aggregate
+
+
+def load_monitored_content() -> list[str]:
+    with Path(MONITORED_CONTENT_FNAME).open("r", encoding="utf-8") as f:
+        return json.load(f)
